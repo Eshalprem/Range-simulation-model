672f2eef14550936e538ba5667de0cd526c7a362
Eshalprem <147090601+Eshalprem@users.noreply.github.com>
2023-11-12 10:32:42 +0530

    Add files via upload

---
+++ MIDC_CODE.m
@@ -0,0 +1,32 @@
+% Given details 
+m = 1200; % Mass
+g = 9.81; % gravitaional force  
+
+Cd =	0.3; 
+Aera =	2.5; % Frontal area 
+Air_Velocity =	0;
+r = 	0.27; % Wheel Radius 
+Grade =	0; % in dgree
+
+CRR =	10; % Co-efficient of rolling resistance 
+
+imf = 1.035; % inertial mass factor 
+mi = imf * m; % beta
+
+GR = 10; % Gear Ratio 
+trans_Eff =	0.93; %Transmission efficiency 
+motor_Eff = 0.8; % Motor + inverter efficiency	
+
+battery_energy = 20;
+DOD = 	0.9 ;% Depth of Discharge 
+battery_density =	100; % Battery energy density
+
+% Ambient temperature 25°C and 
+% pressure of 101.325 kPa
+
+rho =	1.17692 ; % Air density 
+
+% simulnik 
+simtime = 1180;
+drive_cycle = xlsread('MIDC_1180.xlsx'); %#ok<XLSRD>
+sim MIDC_CYCLE.slx;
